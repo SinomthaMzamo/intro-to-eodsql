8ded283b5e39efcbfa800af830359e0a71e9a9a8
Sinomtha Mzamo <167751259+SinomthaMzamo@users.noreply.github.com>
2024-08-19 19:52:58 +0200

    Update PersonDAI.java HOPEFULLY ALL IS WELL NOW, ELSE RIP

--- eodsql-quickstart/src/main/java/ayakhanya/group/id/database/PersonDAI.java
+++ eodsql-quickstart/src/main/java/ayakhanya/group/id/database/PersonDAI.java
@@ -1,5 +1,6 @@
 package ayakhanya.group.id.database;
 import net.lemnik.eodsql.*;
+import java.util.List;
 
 /**
  * Data Access Interface for performing CRUD operations on Person entities.
@@ -7,35 +8,42 @@ import net.lemnik.eodsql.*;
 public interface PersonDAI extends BaseQuery{
 
     // create new person entry (C) using PersonDO fields
-    @Update("INSERT INTO persons (pId, pName, pAge, pGender)"
+    @Update("INSERT INTO persons (name, age, gender)"
             + "VALUES (?{1}, ?{2}, ?{3})"
     )
     void createNewPerson(String personName, int personAge, char personGender);
 
     // edit existing person entry (U)
     @Update("UPDATE persons "
-            + "SET pName = ?{2} "
-            +"WHERE pId = ?{1} "
+            + "SET name = ?{2} "
+            +"WHERE id = ?{1} "
     )
     void updatePersonName(int personId, String newName);
 
     // edit existing person entry (U)
     @Update("UPDATE persons "
-            + "SET pAge = ?{2} "
-            +"WHERE pId = ?{1} "
+            + "SET age = ?{2} "
+            +"WHERE id = ?{1} "
     )
     void updatePersonAge(int personId, int newAge);
 
     // extract person(s) entry(ies) (R)
     @Select("SELECT count(*) FROM friends "
-            + "WHERE fId = {1} "
+            + "WHERE id = {1} "
     )
     int getNumberOfFriends(int friendId);
 
     // extract PersonDO from database persons table
-    @Select("SELECT p.id, p.pName, p.pAge, p.pGender "
+    @Select("SELECT p.id, p.name, p.age, p.gender "
         + "FROM persons p "
         + "WHERE p.id = ?{1}")
     PersonDO getPerson(int personId);
+
+    // extract all FriendDOs with specified friend id
+    @Select("SELECT f.id, f.friendId, f.name, f.gender "
+        + "FROM friends f "
+        + "WHERE f.friendId = ?{1}")
+    List<FriendDO> getFriendsByFriendId(int friendId);
+    
     // delete entry(ies)
 }
