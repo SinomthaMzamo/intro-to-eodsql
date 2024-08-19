66c31a9b43c0a85b550d89961094ecf3a1cabafc
Sinomtha Mzamo <167751259+SinomthaMzamo@users.noreply.github.com>
2024-08-19 19:23:31 +0200

    Update PersonDAI.java to correctly use the ResultColumn defined fields

--- eodsql-quickstart/src/main/java/ayakhanya/group/id/database/PersonDAI.java
+++ eodsql-quickstart/src/main/java/ayakhanya/group/id/database/PersonDAI.java
@@ -7,31 +7,35 @@ import net.lemnik.eodsql.*;
 public interface PersonDAI extends BaseQuery{
 
     // create new person entry (C) using PersonDO fields
-    @Update("INSERT INTO persons (id, name, age, gender)"
+    @Update("INSERT INTO persons (pId, pName, pAge, pGender)"
             + "VALUES (?{1}, ?{2}, ?{3})"
     )
     void createNewPerson(String personName, int personAge, char personGender);
 
     // edit existing person entry (U)
     @Update("UPDATE persons "
-            + "SET name = ?{2} "
-            +"WHERE id = ?{1} "
+            + "SET pName = ?{2} "
+            +"WHERE pId = ?{1} "
     )
     void updatePersonName(int personId, String newName);
 
     // edit existing person entry (U)
     @Update("UPDATE persons "
-            + "SET age = ?{2} "
-            +"WHERE id = ?{1} "
+            + "SET pAge = ?{2} "
+            +"WHERE pId = ?{1} "
     )
     void updatePersonAge(int personId, int newAge);
 
     // extract person(s) entry(ies) (R)
     @Select("SELECT count(*) FROM friends "
-            + "WHERE friendId = {1} "
+            + "WHERE fId = {1} "
     )
     int getNumberOfFriends(int friendId);
 
-
+    // extract PersonDO from database persons table
+    @Select("SELECT p.id, p.pName, p.pAge, p.pGender "
+        + "FROM persons p "
+        + "WHERE p.id = ?{1}")
+    PersonDO getPerson(int personId);
     // delete entry(ies)
 }
